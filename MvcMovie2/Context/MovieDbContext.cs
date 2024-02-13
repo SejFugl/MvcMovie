7d7b824cfbf11632f6e296c7bedcfd20ab43ef7b
SejFugl <erse54105@edu.ucl.dk>
2024-02-13 12:34:15 +0100

    Add project files.

---
+++ MvcMovie2/Context/MovieDbContext.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MvcMovie2.Models;
+
+namespace MvcMovie2.Context
+{
+    public class MovieDbContext : DbContext
+    {
+        public MovieDbContext(DbContextOptions<MovieDbContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        public DbSet<Movie> Movies { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Movie>().HasData(
+                 new Movie
+                 {
+                     Id = 1,
+                     Title = "When Harry Met Sally",
+                     ReleaseDate = DateTime.Parse("1989-2-12"),
+                     Genre = "Romantic Comedy",
+                     Price = 7.99M
+                 },
+
+                    new Movie
+                    {
+                        Id = 2,
+                        Title = "Ghostbusters ",
+                        ReleaseDate = DateTime.Parse("1984-3-13"),
+                        Genre = "Comedy",
+                        Price = 8.99M
+                    },
+
+                    new Movie
+                    {
+                        Id = 3,
+                        Title = "Ghostbusters 2",
+                        ReleaseDate = DateTime.Parse("1986-2-23"),
+                        Genre = "Comedy",
+                        Price = 9.99M
+                    },
+
+                    new Movie
+                    {
+                        Id = 4,
+                        Title = "Rio Bravo",
+                        ReleaseDate = DateTime.Parse("1959-4-15"),
+                        Genre = "Western",
+                        Price = 3.99M
+                    }
+                );
+        }
+    }
+}
---
+++ MvcMovie2/Models/Movie.cs
@@ -0,0 +1,11 @@
+﻿namespace MvcMovie2.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public string Genre { get; set; }
+        public decimal Price { get; set; }
+        public string Title { get; set; }
+    }
+}
