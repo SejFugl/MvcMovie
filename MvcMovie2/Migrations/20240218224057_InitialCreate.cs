8681ef55132a709cf2459aaf539e57c158e779eb
SejFugl <erse54105@edu.ucl.dk>
2024-02-19 00:19:43 +0100

    Migrations from EF Core Tutorial

---
+++ MvcMovie2/Migrations/20240218224057_InitialCreate.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace MvcMovie2.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Movies",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    ReleaseDate = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    Genre = table.Column<string>(type: "TEXT", nullable: false),
+                    Price = table.Column<decimal>(type: "TEXT", nullable: false),
+                    Title = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Movies", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Movies",
+                columns: new[] { "Id", "Genre", "Price", "ReleaseDate", "Title" },
+                values: new object[,]
+                {
+                    { 1, "Romantic Comedy", 7.99m, new DateTime(1989, 2, 12, 0, 0, 0, 0, DateTimeKind.Unspecified), "When Harry Met Sally" },
+                    { 2, "Comedy", 8.99m, new DateTime(1984, 3, 13, 0, 0, 0, 0, DateTimeKind.Unspecified), "Ghostbusters " },
+                    { 3, "Comedy", 9.99m, new DateTime(1986, 2, 23, 0, 0, 0, 0, DateTimeKind.Unspecified), "Ghostbusters 2" },
+                    { 4, "Western", 3.99m, new DateTime(1959, 4, 15, 0, 0, 0, 0, DateTimeKind.Unspecified), "Rio Bravo" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Movies");
+        }
+    }
+}
--- MvcMovie2/Models/Movie.cs
+++ MvcMovie2/Models/Movie.cs
@@ -1,11 +1,20 @@
-﻿namespace MvcMovie2.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MvcMovie2.Models
 {
     public class Movie
     {
         public int Id { get; set; }
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [Display(Name = "Release Date")]
         public DateTime ReleaseDate { get; set; }
+        [StringLength(50)]
         public string Genre { get; set; }
         public decimal Price { get; set; }
+        [Required]
+        [StringLength(50, MinimumLength = 2)]
+        [DisplayFormat(NullDisplayText = "No title")]
         public string Title { get; set; }
     }
 }
